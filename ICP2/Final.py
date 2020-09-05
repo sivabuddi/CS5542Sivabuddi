3bd3e8bb8b03cbfa32c0bdac7dc96ff35b0fbe4a
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2020-09-04 19:14:29 -0500

    ICP2

--- ICP2/Final.py
+++ ICP2/Final.py
@@ -14,7 +14,7 @@ print(lower_words.count())
 # It returns a new RDD by applying a function to each element of the RDD.   Function in map can return only one item.
 result_groupby = lower_words.map(lambda x: (x[0], x)).groupByKey().map(lambda x: (x[0], list(x[1])))
 print("--------------------Using groupby-----------------------------------------------------")
-# Retreive the elements one by one using foreach loop
+# Retreive the elements one by one using foreach loop using spark action called collect
 for each_groupby in result_groupby.collect():
     print(each_groupby)
 result_reducedby = lower_words.map(lambda x: (x[0], x)).reduceByKey(lambda x, y: x + " , " + y)
