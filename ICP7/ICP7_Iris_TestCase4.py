a0a678b0f4615c69c7aaa575cc185e0d813bde21
sivabuddi <sivakumar.umkc.fall2019@gmail.com>
2020-10-30 19:45:33 -0500

    reinformenet learning

---
+++ ICP7/ICP7_Iris_TestCase4.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+sns.set(style="white", color_codes=True)
+import warnings
+warnings.filterwarnings("ignore")
+
+dataset = pd.read_csv('Iris.csv')
+print("Original Data size=",dataset.shape)
+# print(dataset.describe())
+print(dataset.columns)
+x = dataset.iloc[:,[1,3]]
+y = dataset.iloc[:,-1]
+# see how many samples we have of each species
+print(dataset["Species"].value_counts())
+
+
+##elbow method to know the number of clusters
+wcss = []
+
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i,init='k-means++',max_iter=300,n_init=10,random_state=0)
+    kmeans.fit(x)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11),wcss)
+plt.title('Elbow Curve for Iris Dataset')
+plt.xlabel('Number of Clusters')
+plt.ylabel('Wcss')
+plt.show()
+
+
+# Set style of scatterplot
+sns.set_context("notebook", font_scale=1.1)
+sns.set_style("ticks")
+
+# Create scatterplot of dataframe
+sns.lmplot('SepalLengthCm', # Horizontal axis
+           'PetalLengthCm', # Vertical axis
+           data=dataset, # Data source
+           fit_reg=False, # Don't fix a regression line
+           hue="Species", # Set color
+           scatter_kws={"marker": "D", # Set marker style
+                        "s": 100}) # S marker size
+
+# Set title
+plt.title('Iris Dataset')
+
+# Set x-axis label
+plt.xlabel('SepalLenghtCm')
+
+# Set y-axis label
+plt.ylabel('PetalLengthCm')
+plt.show()
+
+
+
+# sns.FacetGrid(dataset, hue="Species", size=4).map(plt.scatter, "SepalLengthCm", "SepalWidthCm").add_legend()
+# sns.lmplot(x, y, data=x, hue="hue")
+
+# # do same for petals
+# sns.FacetGrid(dataset, hue="Species", size=4).map(plt.scatter, "PetalLengthCm", "PetalWidthCm").add_legend()
+# #
+# sns.FacetGrid(dataset, hue="Species", size=4).map(plt.scatter, "SepalLengthCm", "PetalLengthCm").add_legend()
+# plt.show()
+
+from sklearn import preprocessing
+scaler = preprocessing.StandardScaler()
+scaler.fit(x)
+X_scaled_array = scaler.fit_transform(x)
+X_scaled = pd.DataFrame(X_scaled_array, columns = x.columns)
+
+from sklearn.cluster import KMeans
+nclusters = 3 # this is the k in kmeans
+km = KMeans(n_clusters=nclusters)
+km.fit(X_scaled_array)
+pred = km.predict(X_scaled_array)
+X_scaled['clusters']=pred
+result = X_scaled['clusters'].value_counts()
+print(result)
+print(pred)
+
+#6 Visualising the clusters
+plt.scatter(X_scaled_array[pred==0, 0], X_scaled_array[pred==0, 1], s=100, c='red', label ='Cluster 1')
+plt.scatter(X_scaled_array[pred==1, 0], X_scaled_array[pred==1, 1], s=100, c='blue', label ='Cluster 2')
+plt.scatter(X_scaled_array[pred==2, 0], X_scaled_array[pred==2, 1], s=100, c='green', label ='Cluster 3')
+# plt.scatter(X_scaled_array[pred==3, 0], X_scaled_array[pred==3, 1], s=100, c='black', label ='Cluster 4')
+plt.legend()
+plt.title("Predicated Clusters ")
+plt.xlabel("SepalLengthCm")
+plt.ylabel("PetalLengthCm")
+plt.show()
+
+
+# predict the cluster for each data point
+# y_cluster_kmeans = km.predict(x)
+from sklearn import metrics
+score = metrics.silhouette_score(X_scaled_array, pred)
+print(score)
+
+
+
